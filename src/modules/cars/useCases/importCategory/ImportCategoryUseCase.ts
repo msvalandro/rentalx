45ec6b0110185bf127022e0546b48257cf59a65f
Matheus Valandro <msvalandro@gmail.com>
2021-10-28 20:42:45 -0300

    add swagger documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -29,7 +29,10 @@ class ImportCategoryUseCase {
 
           categories.push({ name, description });
         })
-        .on('end', () => resolve(categories))
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
         .on('error', err => reject(err));
     });
   }
